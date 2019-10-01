a5a4c832e3cd4fe562c437bc3198a432d8396dc6
Carolineolivia <carolineolivia@prolike.io>
2019-10-01 19:33:26 +0200

    bundler

--- devserve.sh
+++ devserve.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
+rm -rf Gemfile.lock
+echo 'export BUNDLER_VERSION=$(cat Gemfile.lock | tail -1 | tr -d " ")' >> $BASH_ENV
+source $BASH_ENV
+gem install bundler
+bundle install
 
 docker run \
   -it \
@@ -7,4 +12,4 @@ docker run \
   -v /$(pwd)://app:rw \
   --workdir //app  \
   --publish 80:4000 \
-  lakruzz/jekyll-plus:latest jekyll serve --config _config.yml,_dev_config.yml --watch
+  lakruzz/jekyll-plus:latest bundle exec jekyll serve --config _config.yml,_dev_config.yml --watch
